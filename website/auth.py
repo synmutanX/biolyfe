cd7b5dfb76ed92b419738bc1a6591429f1772f57
synmutanX <maulanasofyan0712@gmail.com>
2024-10-15 06:30:14 +0700

    feat: update route models template and requirements

---
+++ website/auth.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return 'This is the login page'
+
+
+@auth.route('/sign-up')
+def sign_up():
+    return 'This is the sign up page'
